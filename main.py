b4867012c0c4d93c2d76b1eb73e22d8a02db3fdd
signusfalcon <signus.falcon@gmail.com>
2021-02-07 16:17:59 +0800

    Added a few helpful comments

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Process import arrange
 
 # Initialize process_amount as 0
 process_amount = 0
-processes = []
+processes = list()
 
 arrival_time_zero = False
 
@@ -17,7 +17,11 @@ arrival_time_zero = False
 while True:
     try:
         process_amount = int(input('Enter number of processes: '))
-        break
+        if process_amount < 2:
+            print("There should be at least 2 processes!")
+            continue
+        else:
+            break
     except ValueError:
         print("Please enter a process amount from 2-5\n")
         continue
@@ -33,7 +37,6 @@ for amount in range(1, process_amount + 1):
     # It is inefficient in python, but it is efficient if implemented in a lower level language
     while True:
         try:
-            finished_state = False
             arrival_time = int(input(f"Enter arrival time of process {str(amount)}: "))
             if arrival_time == 0:
                 arrival_time_zero = True
@@ -53,65 +56,45 @@ for amount in range(1, process_amount + 1):
 
 # First, sort the processes according to arrival time.
 processes.sort(key=lambda x: x.arrival_time)
-waiting_queue = list()
+
+# Initialize ready_queue, process_queue and current_time
 ready_queue = list()
 process_queue = list()
-
-start_time = list()
-exit_time = list()
 current_time = 0
 
 # List of processes in a sequence.
 # Works like a timestamp of processes.
-# FOR SOME ODD REASON.
 sequence_of_process = list()
 
 # This is where the processing begins:
 while True:
     # Check if a process / processes needs to put to the waiting_queue
-    #print("Time: " + str(current_time))
-    # _ = input("Press any key to continue...")
-
+    # If process inside processes list matches the current time, insert it to the ready_queue
     index_to_remove = list()
     for process in processes:
         if process.arrival_time == current_time:
             index_to_remove.append(processes.index(process))
             ready_queue.append(process)
 
+    # After the processes are appended, these have to be removed from the processes list.
     for index in sorted(index_to_remove, reverse=True):
         processes.pop(index)
 
-    # DEBUG
-    #print("Processes left")
-    #for process in processes:
-    #    print(process)
-
-    #print("Processes inside ready queue:")
-    #for process in ready_queue:
-    #    print(process)
-
-
-    # _ = input("Press any key to continue...")
-
     # Check if length of waiting queue is more than 1
     if len(ready_queue) >= 1:
+        # Sort the ready_queue according to priority
         ready_queue.sort(key=lambda x: x.priority)
         # Group the processes according to priority then sort each group according to process_id
         ready_queue = arrange(ready_queue)
 
-    # _ = input("Press any key to continue...")
-
+    # If the process_queue is empty, check if the ready_queue is empty.
     if len(process_queue) == 0:
+        # If ready queue is not empty, then insert the first index inside the ready queue
         if len(ready_queue) != 0:
             ready_queue[0].submission_time = current_time
             process_queue.append(ready_queue.pop(0))
 
-        # DEBUG
-        #print("Process queue: \n" + str(process_queue[0]))
-        #print("Processes left inside ready_queue: ")
-        #for process in ready_queue:
-        #    print(process)
-
+    # Unquote this if you want pre-emptive priority scheduling
     """elif len(process_queue) == 1 and len(ready_queue) != 0:
         if ready_queue[0].priority < process_queue[0].priority:
             process_queue[0].completion_time = current_time
@@ -127,31 +110,17 @@ while True:
             process_queue.append(ready_queue.pop(0))
             ready_queue.insert(0, process_queue.pop(0))"""
 
-
-            # DEBUG
-            #print("Process queue: \n" + str(process_queue[0]))
-            #print("Processes left inside ready_queue: ")
-            #for process in ready_queue:
-            #    print(process)
-
-    # _ = input("Press any key to continue...")
-    # DEBUG
-    #print("Before process queue remaining burst time: " + str(process_queue[0].remaining_burst_time))
-
     if len(process_queue) == 1:
         process_queue[0].remaining_burst_time -= 1
 
     current_time += 1
 
-    # DEBUG
-    #print("After process queue remaining burst time: " + str(process_queue[0].remaining_burst_time))
-
-    # _ = input("Press any key to continue...")
-
     # If the burst time remaining is 0, then it should swapped out of process_queue
     # Then append the completed process to sequence_of_process
     if len(process_queue) == 1 and process_queue[0].remaining_burst_time == 0:
         process_queue[0].completion_time = current_time
+        # Copy the Process object to another Process object for a saved instance of the completed Process
+        # be appended inside the sequence_of_process list.
         completed_process = Process(process_queue[0].process_id,
                                     process_queue[0].arrival_time,
                                     process_queue[0].burst_time,
@@ -162,25 +131,22 @@ while True:
         process_queue.pop(0)
         sequence_of_process.append(completed_process)
 
-        # DEBUG
-
-
         # Insert first index process inside waiting queue
         if len(ready_queue) != 0:
             ready_queue[0].submission_time = current_time
             process_queue.append(ready_queue.pop(0))
+        # If the ready queue, process queue and processes list (or waiting queue) are empty, it means it is finished
+        # Then the outermost while loop should be broken to output the gantt chart and the final results
         elif len(ready_queue) == 0 and len(process_queue) == 0 and len(processes) == 0:
             break
 
-    # _ = input("Press any key to continue...")
-
+# Output the GANTT Chart
 print("\n\nGantt Chart: ")
 print(GanttChart(sequence_of_process))
 print("\n\n")
 
+# Output the Utilization, TAT, ART, AWT.
 print(f"CPU Utilization: {cpu_utilization(sequence_of_process, arrival_time_zero)}")
 print(f"Average turnaround time: {average_turnaround_time(sequence_of_process)}")
 print(f"Average response time: {average_response_time(sequence_of_process)}")
 print(f"Average waiting time: {average_waiting_time(sequence_of_process)}")
-
-
